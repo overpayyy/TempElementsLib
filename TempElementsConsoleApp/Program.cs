aa5a8b275763e557dfc7d0437d7237f7dfb2521e
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-30 18:14:41 +0200

    Add project files.

---
+++ TempElementsConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TempElementsConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
